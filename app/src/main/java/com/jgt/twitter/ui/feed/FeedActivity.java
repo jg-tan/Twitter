454d72f6aafd2f51e532973fbcb9d8c9ace8a7f7
Joseph <tan.joseph.gabriel@gmail.com>
2021-04-02 10:49:19 +0800

    Complete UI TODO: functionality

--- app/src/main/java/com/jgt/twitter/ui/feed/FeedActivity.java
+++ app/src/main/java/com/jgt/twitter/ui/feed/FeedActivity.java
@@ -6,13 +6,23 @@ import com.jgt.twitter.R;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class FeedActivity extends AppCompatActivity {
     private static final String TAG = FeedActivity.class.getSimpleName();
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        toolbar = findViewById(R.id.tbFeed);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
